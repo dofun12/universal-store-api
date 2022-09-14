674e0b2f1c0fc6036cc658cbb2574285b8d1b8f4
kvnsu <k.v.n.such@gmail.com>
2022-09-14 13:51:22 -0300

    Generic save

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 from bson.json_util import dumps, loads
 
 import os
-
+import uuid
 
 # Now you can perfom any CRUD operations on the DB
 # testes
@@ -32,38 +32,86 @@ class DBManager:
         print(connection_url)
         self.client = MongoClient(connection_url)
 
-    def toJson(self, object):
-        return object
+    def toJson(self, json_object, format_json=False):
+        if format_json:
+            return json.loads(dumps(json_object))
+        return json_object
 
     def list_all(self, db_name: str, collection: str):
         db = self.client[db_name]
         collection = db[collection]
         cursor = collection.find()
         list_cur = list(cursor)
-        return self.toJson(json.loads(dumps(list_cur)))
+        return self.toJson(list_cur, True)
 
     def insert(self, db_name: str, collection: str, data: dict):
         db = self.client[db_name]
         collection = db[collection]
-        saved = collection.find_one(data)
         try:
             collection.insert_one(data)
-            return self.toJson({'response': 'Saved', 'success': True, 'data': saved})
+            saved = collection.find_one(data)
+            return self.toJson({'response': 'Saved', 'success': True, 'saved': self.toJson(saved, True)})
         except Exception as e:
             print(e)
             return self.toJson({'response': 'Can\'t save', 'success': False})
 
+    def store_item(self, data: dict):
+        db = self.client['generics']
+        collection = db['objects']
+        uuid_str = str(uuid.uuid4())
+        generic_data = {
+            "uuid": uuid_str,
+            "data": data
+        }
+        try:
+            result = collection.insert_one(generic_data)
+            if not result.acknowledged:
+                return self.toJson({'response': 'Can\'t save', 'success': False})
+            return self.toJson({'response': 'Saved', 'success': True, 'uuid': uuid_str})
+        except Exception as e:
+            print(e)
+            return self.toJson({'response': 'Can\'t save', 'success': False})
+
+    def get_item(self, uuid_str: str):
+        db = self.client['generics']
+        collection = db['objects']
+
+        filter_obj = {
+            "uuid": uuid_str
+        }
+        try:
+            founded = collection.find_one(filter_obj)
+            return self.toJson({'response': 'Founded', 'success': True, 'data': self.toJson(founded, True)})
+        except Exception as e:
+            print(e)
+            return self.toJson({'response': 'Can\'t save', 'success': False})
+
+    def update_item(self, uuid_str: str, data: dict):
+        db = self.client['generics']
+        collection = db['objects']
+
+        filter_obj = {
+            "uuid": uuid_str
+        }
+        try:
+            collection.update_one(filter_obj, {"$set": {"data": data}})
+            founded = collection.find_one(filter_obj)
+            return self.toJson({'response': 'Founded', 'success': True, 'data': self.toJson(founded, True)})
+        except Exception as e:
+            print(e)
+            return self.toJson({'response': 'Can\'t update', 'success': False})
+
     def get_last_topic(self, topic: str):
         db = self.client['mqtt']
         collection = db['topics']
-        filter = {'topic': topic}
-        return json.loads(dumps(collection.find_one(filter)))
+        filter_obj = {'topic': topic}
+        return self.toJson(collection.find_one(filter_obj), True)
 
     def save_last_topic(self, topic: str, data: dict):
         db = self.client['mqtt']
         collection = db['topics']
-        filter = {'topic': topic}
-        cursor = collection.find_one(filter)
+        filter_obj = {'topic': topic}
+        cursor = collection.find_one(filter_obj)
         if cursor is None:
             try:
                 collection.insert_one(data)
@@ -73,7 +121,7 @@ class DBManager:
                 return self.toJson({'response': 'Can\'t save', 'success': False})
 
         try:
-            collection.update_one(filter, {"$set": data})
+            collection.update_one(filter_obj, {"$set": data})
             return self.toJson({'response': 'Saved', 'success': True, 'data': data})
         except Exception as e:
             print(e)
--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, Request
 from src.database.db_manager import DBManager
 
 app = FastAPI()
-
+db = DBManager()
 
 @app.get("/")
 async def root():
@@ -19,9 +19,15 @@ async def say_hello(name: str):
 
 @app.get("/store/{db_name}/{collection}")
 async def list_all(db_name: str, collection: str):
-    const = DBManager().list_all(db_name, collection)
+    const = db.list_all(db_name, collection)
     return const
 
+
+@app.post("/store/{db_name}/{collection}")
+async def insert(db_name: str, collection: str, info: Request):
+    dataJson = await info.json()
+    const = db.insert(db_name, collection, dataJson)
+    return const
 
 @app.post("/topic/last")
 async def save_topic(info: Request):
@@ -29,11 +35,30 @@ async def save_topic(info: Request):
     print(data_json)
     if data_json['topic'] is not None:
         return DBManager().save_last_topic(data_json['topic'], data_json)
-    return DBManager().save_last_topic('none', data_json)
+    return db.save_last_topic('none', data_json)
+
+
+@app.post("/generic/item")
+async def save_item(info: Request):
+    data_json = await info.json()
+    return db.store_item(data_json)
+
+
+@app.put("/generic/item/{uuid}")
+async def save_item(uuid: str, info: Request):
+    data_json = await info.json()
+    return db.update_item(uuid, data_json)
+
+
+@app.get("/generic/item/{uuid}")
+async def get_item(uuid: str):
+    return db.get_item(uuid)
+
 
 @app.get("/topic/last")
 async def get_topic(topic: str = 'none'):
-    if topic is 'none':
+    if topic == 'none':
         return {'status': 'none'}
-    return DBManager().get_last_topic(topic)
+    return db.get_last_topic(topic)
+
 
