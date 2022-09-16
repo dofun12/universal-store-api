9643774e7cdfedbc4d3b8ce62d667cbcfefc50aa
kvnsu <k.v.n.such@gmail.com>
2022-09-16 11:51:50 -0300

    Cors e cors corssssss

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from src.routes.api import router as api_router
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+
 
 
 def get_application() -> FastAPI:
@@ -14,3 +16,14 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
+origins = [
+    '*'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
